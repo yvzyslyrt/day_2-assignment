958acc3659be4a97ccb76ff89ded78bddd8141e1
yvzyslyrt <65642458+yvzyslyrt@users.noreply.github.com>
2020-05-20 05:21:36 +0100

    day2 assignment

---
+++ Question3.java
@@ -0,0 +1,10 @@
+package day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		System.out.println("1\r2\r3\r4\r5\r6\r7\r8\r9\r10");
+
+	}
+
+}
---
+++ Question7.java
@@ -0,0 +1,14 @@
+package day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" /\\  /\\ ");
+		System.out.println("/__\\/__\\");
+		System.out.println("ý am from \"germany\".");
+		System.out.println("how is your day going on, \'good\' or \'bad\'?");
+
+	}
+
+}
